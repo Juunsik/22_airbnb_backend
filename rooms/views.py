1d856757fb4fa70fd62a9f661bee01c93c7e6715
Heo-JunHyeok <junhyeok_1996@naver.com>
2022-10-25 00:52:01 +0900

    #11.15 File Uploads

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from rest_framework.views import APIView
 from django.db import transaction
 from rest_framework.response import Response
@@ -216,7 +217,7 @@ class RoomAmenities(APIView):
             page = int(request.query_params.get("page", 1))
         except ValueError:
             page = 1
-        page_size = 3
+        page_size = settings.PAGE_SIZE
         start = (page - 1) * page_size
         end = start + page_size
         room = self.get_object(pk)
@@ -225,3 +226,8 @@ class RoomAmenities(APIView):
             many=True,
         )
         return Response(serializer.data)
+
+
+class RoomPhotos(APIView):
+    def post(self, request, pk):
+        pass
